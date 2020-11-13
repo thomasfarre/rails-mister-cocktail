4564ca34435ed3cf0b6f5b2b455273127fbd0570
thomasfarre <th.farre@gmail.com>
2020-11-13 18:05:30 +0100

    test

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,5 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   has_one_attached :photo
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
